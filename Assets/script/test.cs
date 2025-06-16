765d1a57fe21736780935a73bace7d9f2853aa6e
enzzo95 <soler.enzo@gmail.com>
2025-06-16 15:09:45 +0200

    First commit

---
+++ Assets/script/test.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
