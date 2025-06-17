e8c1052e158eb673eddf8cc9a82b310b816bd7e0
Eden <eden.vandewatyne@estiam.com>
2025-06-17 14:56:17 +0200

    Addition of the collision mechanic during attack

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,22 @@ public class Player : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 10f;
+    public string opponentTag = "J2";
 
     public KeyCode left = KeyCode.A;
     public KeyCode right = KeyCode.D;
     public KeyCode jump = KeyCode.Space;
+    public KeyCode attack = KeyCode.E;
 
     private Rigidbody2D rb;
     private bool isOnGround = false;
+    private bool isTouchingOpponent = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
@@ -41,22 +45,43 @@ public class Player : MonoBehaviour
         }
 
         rb.linearVelocity = new Vector2(move.x * speed, rb.linearVelocity.y);
+
+        if (isTouchingOpponent && Input.GetKeyDown(attack)) 
+        {
+            Debug.Log(gameObject.tag + " collide " + opponentTag);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        foreach (var contact in collision.contacts)
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            if (contact.normal.y > 0.5f)
+            foreach (var contact in collision.contacts)
             {
-                isOnGround = true;
-                break;
+                if (contact.normal.y > 0.5f)
+                {
+                    isOnGround = true;
+                    break;
+                }
             }
+        }
+
+        if (collision.gameObject.CompareTag(opponentTag))
+        {
+            isTouchingOpponent = true;
         }
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        isOnGround = false;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = false;
+        }
+
+        if (collision.gameObject.CompareTag(opponentTag))
+        {
+            isTouchingOpponent = false;
+        }
     }
 }
