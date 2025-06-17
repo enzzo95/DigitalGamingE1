658c665127e2108f3141bc195f27db2c4aaa8047
Eden <eden.vandewatyne@estiam.com>
2025-06-17 20:00:11 +0200

    Added ArmAttack script for collision detection

---
+++ Assets/Scripts/ArmAttack.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ArmAttack : MonoBehaviour
+{
+    public string opponentTag = "J2";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(opponentTag))
+        {
+            Debug.Log("Arm touched " + opponentTag);
+        }
+    }
+}
