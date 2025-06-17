8a6021fed060e2e80583251b7bf0a461aef1a9eb
Eden <eden.vandewatyne@estiam.com>
2025-06-17 12:21:41 +0200

    Addition of the jumping mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,19 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 10f;
 
     public KeyCode left = KeyCode.A;
     public KeyCode right = KeyCode.D;
+    public KeyCode jump = KeyCode.Space;
+
+    private Rigidbody2D rb;
+    private bool isOnGround = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -29,6 +34,29 @@ public class Player : MonoBehaviour
             move.x += 1f;
         }
 
-        transform.Translate(move * speed * Time.deltaTime);
+        if (Input.GetKeyDown(jump) && isOnGround)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isOnGround = false;
+        }
+
+        rb.linearVelocity = new Vector2(move.x * speed, rb.linearVelocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (var contact in collision.contacts)
+        {
+            if (contact.normal.y > 0.5f)
+            {
+                isOnGround = true;
+                break;
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isOnGround = false;
     }
 }
